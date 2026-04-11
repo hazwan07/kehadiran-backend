a688d865a5b09fda8ce77fb7762197147c2b56f3
hazwan07 <hazwan_danial@yahoo.com>
2026-04-11 08:03:34 +0800

    Feat: Add support for FIREBASE_SERVICE_ACCOUNT_BASE64 to allow Render to authenticate

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -14,7 +14,13 @@ dotenv.config();
 // In production, use GOOGLE_APPLICATION_CREDENTIALS env var
 // pointing to service-account.json
 if (!admin.apps.length) {
-  if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+  if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+    const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8'));
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'kehadiran-dev.appspot.com',
+    });
+  } else if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
     admin.initializeApp({
       credential: admin.credential.applicationDefault(),
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
